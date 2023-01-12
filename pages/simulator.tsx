7fa83f0e358957dece4b42d51d7e8b4f1247e0a4
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2023-01-13 04:12:22 +0530

    Maintain session for each simulation request

--- pages/simulator.tsx
+++ pages/simulator.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Dashboard from "../components/dashboard";
 import Charts from "../components/charts";
+import { v4 as uuidv4 } from "uuid";
 
 const Simulator = () => {
+  useEffect(() => {
+    let data = sessionStorage.getItem("sessionid");
+
+    if (!data) {
+      sessionStorage.setItem("sessionid", uuidv4());
+    }
+  }, []);
+
   return (
     <div>
-      {" "}
       <Dashboard />
       <Charts />
     </div>
