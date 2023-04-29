626dc028f0719d04087113243e631adc8525f663
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2023-04-29 01:14:55 -0400

    add how to

--- pages/how-to-use-simulator.tsx
+++ pages/how-to-use-simulator.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const HowToUse = () => {
   return (
-    <div className="prose container mx-auto">
+    <div className="prose mx-auto">
       <div className="flex justify-between my-4">
         <Link href={"/"}>
           <span className="link link-primary">← Home</span>
@@ -25,6 +26,16 @@ const HowToUse = () => {
         is reproductively isolated from the other strains and they cannot
         exchange genetic material.
       </p>
+      <div className="mx-auto w-full">
+        <Image
+          width={800}
+          height={500}
+          src={"/assets/mutual1.svg"}
+          alt="cells1"
+          className=""
+        />
+      </div>
+
       <p>
         In this diagram you see two different types of yeast strains that are
         trading resources through the surrounding media in which they grow. The
@@ -41,9 +52,13 @@ const HowToUse = () => {
         other nutrients in the media such as glucose, and when populations sizes
         are large they may start competing for these other nutrients (|--|).
       </p>
-
+      <Image
+        width={1200}
+        height={600}
+        src={"/assets/mutual2.svg"}
+        alt="cells1"
+      />
       <p>
-        {" "}
         There are also cheater strains that consume either adenine or lysine but
         do not overproduce either of these resources. You can think of these as
         direct competitors with each of the mutualist strains. For example, the
@@ -71,6 +86,8 @@ const HowToUse = () => {
         </li>
       </ul>
 
+      <h2>Usage</h2>
+
       <p>
         For example, you could start a community with just one strain of each
         mutualist type—1 adeOP, 1 lysOP—to make a pairwise mutualism and study
@@ -78,30 +95,97 @@ const HowToUse = () => {
         mutualist type and each of the cheater strains.
       </p>
 
-      <p>
-        ou designate your starting community by using pull down menus to choose
-        the number of strains and a slider to choose population size. In this
-        example there are 2 strains of AdeOPs, 2 strains of lysOPs each starting
-        with 65 individual cells, and 1 cheater strain that takes adenine and 1
-        cheater strain that takes lysine each starting with 10 individual cells.
-      </p>
+      <h3>1. Initialize community</h3>
+      <div className="flex items-start">
+        <Image
+          width={500}
+          height={600}
+          src={"/assets/controls1.png"}
+          alt="controls1"
+          className="object-cover rounded shadow"
+        />
+        <p className="w-10/12 px-6 mt-0">
+          You designate your starting community by using pull down menus to
+          choose the number of strains and a slider to choose population size.
+          In this example there are 2 strains of AdeOPs, 2 strains of lysOPs
+          each starting with 65 individual cells, and 1 cheater strain that
+          takes adenine and 1 cheater strain that takes lysine each starting
+          with 10 individual cells.
+        </p>
+      </div>
+      <h3>2. Configure Nutrients</h3>
+      <div className="flex items-start">
+        <Image
+          width={550}
+          height={600}
+          src={"/assets/controls2.png"}
+          alt="controls1"
+          className="object-cover rounded shadow"
+        />
+        <div className="w-10/12 px-6 mt-0">
+          <p className="mt-0">
+            You can also change the level of nutrients in the starting media by
+            using sliders. The lower the number the lower the amount of
+            resources already in the media. In this example, the simulator
+            starts with just a little bit of glucose, adenine, and lysine in the
+            media.
+          </p>
+          <div className="alert shadow-lg">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-info flex-shrink-0 w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <div>
+                <p className="">
+                  Keep in mind that the mutualist strains will be adding adenine
+                  or lysine but glucose is not replenished as the cell grows.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h3>3. Set the duration</h3>
+      <div className="flex items-start">
+        <Image
+          width={500}
+          height={200}
+          src={"/assets/controls3.png"}
+          alt="controls1"
+          className="object-cover rounded shadow"
+        />
+        <p className="w-10/12 px-6 mt-0">
+          The last thing that you can select is the number of days you want the
+          community to interact. Typically yeast strains take about 2 hours to
+          go through their life cycle under ideal growing conditions. The
+          strains in the simulator will grow a little slower but you can figure
+          on about 6-8 generations per day.
+        </p>
+      </div>
 
       <p>
-        You can also change the level of nutrients in the starting media by
-        using sliders. The lower the number the lower the amount of resources
-        already in the media. In this example, the simulator starts with just a
-        little bit of glucose, adenine, and lysine in the media. Keep in mind
-        that the mutualist strains will be adding adenine or lysine but glucose
-        is not replenished as the cell grows.
+        As you run the simulator and after it is complete you will see a graph
+        of the number of cells of each strain type.
       </p>
 
-      <p className="mb-16">
-        The last thing that you can select is the number of days you want the
-        community to interact. Typically yeast strains take about 2 hours to go
-        through their life cycle under ideal growing conditions. The strains in
-        the simulator will grow a little slower but you can figure on about 8
-        6-8 generations per day.
-      </p>
+      <Image
+        width={1400}
+        height={500}
+        src={"/assets/dashboard0.png"}
+        alt="cells1"
+        className="object-cover"
+      />
+      {/* END NAVIGATION */}
       <div className="flex justify-between my-8">
         <Link href={"/"}>
           <span className="link link-primary">← Home</span>
